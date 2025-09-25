1b98f840bb9091de548902b88ff591f5c8d8be2b
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2025-09-25 10:47:42 +0200

    license
    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.civitas.logging.handler/src/org/civitas/minio/example/EventHandler.java
+++ org.civitas.logging.handler/src/org/civitas/minio/example/EventHandler.java
@@ -1,8 +1,15 @@
-/*
- * Example Event Handler for MinIO CSV Download Service
+/**
+ * Copyright (c) 2012 - 2025 Data In Motion and others.
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
- * This is an example of how to create an event handler to receive
- * the parsed EObjects from the MinIOCSVDownloadService.
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *     Data In Motion - initial API and implementation
  */
 package org.civitas.minio.example;
 
